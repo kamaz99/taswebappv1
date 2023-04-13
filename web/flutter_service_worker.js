ba3a87368cd51a310ca78452a10c54759dd530e2
kamaz99 <pocelyev.ihor@gmail.com>
2023-04-13 13:20:48 +0300

    Add files via upload

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "507e1120e30c0275509f65a5e0512b00",
-"main.dart.js": "11248aacd26df0be86308d61c36479df",
+"main.dart.js": "4553c42d390e247146c6db0ef813a0a2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "c7f55f31ca9a5e021d1413e2ba567932",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "06266128e0f7cf37f82297909f290fbc",
-"/": "06266128e0f7cf37f82297909f290fbc",
+"index.html": "ab266a7179e334e3b3ad67159aeeb3da",
+"/": "ab266a7179e334e3b3ad67159aeeb3da",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
